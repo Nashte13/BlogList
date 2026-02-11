9e480159f60bdaab785e2045d862bf5b1ba5c776
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-02-11 16:56:23 +0300

    starting blog list app

--- bloglist-backend/index.js
+++ bloglist-backend/index.js
@@ -1,1 +1,39 @@
-console.log('Blog list testing');
+const express = require('express')
+
+const mongoose = require('mongoose')
+
+const app = express()
+
+const blogSchema = mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+const mongoUrl = 'mongodb://localhost/bloglist'
+mongoose.connect(mongoUrl, {family: 4})
+
+app.use(express.json())
+
+app.get('/api/blogs', (req, res) => {
+    Blog.find({}).then(blogs => {
+        res.json(blogs)
+    })
+})
+
+app.post('/api/blogs', (req, res) => {
+    const blog = new Blog(req.body)
+
+    blog.save().then((result) => {
+        res.status(201).json(result)
+    })
+})
+
+const PORT = 3003
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}`)
+})
