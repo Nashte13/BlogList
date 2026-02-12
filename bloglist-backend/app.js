975caad91d2f96f2c5b0dae624d6bb6e45c7733e
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-02-12 15:06:54 +0300

    connecting to db

--- bloglist-backend/app.js
+++ bloglist-backend/app.js
@@ -1,3 +1,25 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Blog = require('./models/blogs')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 const notesRouter = require('./controllers/notes')
+
+const app = express()
+
+logger.info('Connecting to', config.MONGODB_URL)
+
+mongoose
+    .connect(config.MONGODB_URL, {family: 4})
+    .then(() => {
+        logger.info('Connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('Error connecting to MongoDB:', error.message)
+    })
+
+app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use('/api/notes', notesRouter)
