a1f33d5a7daed83ee30f084c17ec7bb8bcb4370f
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-02-23 14:29:28 +0300

    splitting into different modules

--- bloglist-backend/index.js
+++ bloglist-backend/index.js
@@ -13,8 +13,6 @@ mongoose.connect(mongoUrl, {family: 4})
 app.use(express.json())
 
 
-const PORT = 3003
-
 app.listen(config.PORT, () => {
     logger.info(`App running on port ${config.PORT}`)
 })
