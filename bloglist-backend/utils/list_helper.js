31d7af819135e916228d753591ebd044099a3633
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-02-26 13:26:55 +0300

    favorite blog test

--- bloglist-backend/utils/list_helper.js
+++ bloglist-backend/utils/list_helper.js
@@ -6,7 +6,23 @@ const totalLikes = (blogs) => {
     return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+
+    //find blogs with max likes
+    let favorite = blogs[0]
+    blogs.forEach(blog => {
+        if (blog.likes > favorite.likes) {
+            favorite = blog
+        }
+    })
+    return favorite
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
