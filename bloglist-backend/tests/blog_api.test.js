056da468ae04c273c84bc0fcb8e582849aec22f3
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-03-03 15:42:52 +0300

    changing script dependencies

--- bloglist-backend/tests/blog_api.test.js
+++ bloglist-backend/tests/blog_api.test.js
@@ -12,6 +12,21 @@ test('blogs are returned as json', async () => {
         .expect('Content-Type', /application\/json/)
 })
 
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+
+    assert.strictEqual(response.body.length, 2)
+})
+
+test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+
+    const contents = response.body.map(r => r.title)
+    assert.strictEqual(contents.includes('Go To Statement Considered Harmful'), true)
+})
+
+
+
 
 
 after(() => {
