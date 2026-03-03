ffecab61977d75ce23c9119751ee5810ab76d88a
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-03-03 15:50:24 +0300

    initializing hardcoded blogs list

--- bloglist-backend/tests/blog_api.test.js
+++ bloglist-backend/tests/blog_api.test.js
@@ -5,6 +5,22 @@ const app = require('../app')
 
 const api = supertest(app)
 
+const initialBlogs = [
+    {
+        title: "Go To Statement Considered Harmful",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+        likes: 5
+    },
+    {
+        title: "Canonical string reduction",
+        author: "Edsger W. Dijkstra",
+        url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+        likes: 12
+    }
+]
+
+
 test('blogs are returned as json', async () => {
     await api
         .get('/api/blogs')
