fc7171166e31fed4a9be7cb5b276c28e943eb173
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-03-05 13:38:42 +0300

    updating route

--- bloglist-backend/controllers/blog.js
+++ bloglist-backend/controllers/blog.js
@@ -33,6 +33,23 @@ blogsRouter.post('/', async (req, res, next) => {
     
 })
 
+blogsRouter.put('/:id', async(req, res) => {
+    const {likes} = req.body
+
+    const updatedBlog = await Blog.findByIdAndUpdate(
+        req.params.id,
+        {likes},
+        {new: true, runValidators: true, context: 'query'}
+    )
+
+    if (updatedBlog) {
+        res.json(updatedBlog)
+    } else {
+        res.status(404).json({ error: 'blog not found' })
+    }
+    
+})
+
 blogsRouter.delete('/:id', async (req, res) => {
    const deletedBlog =  await Blog.findByIdAndRemove(req.params.id)
    if (!deletedBlog) {
