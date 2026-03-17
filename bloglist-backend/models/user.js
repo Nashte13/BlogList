d0c95019c3cf27fd0cac41a454e0a05cda9dc8a8
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-03-17 12:47:45 +0300

    validations in schema

--- bloglist-backend/models/user.js
+++ bloglist-backend/models/user.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
+        minLength: 3
     },
     name: String,
     passwordHash: {
@@ -24,6 +25,7 @@ userSchema.set('toJSON', {
         returnedObject.id = returnedObject._id.toString();
         delete returnedObject._id;
         delete returnedObject.__v;
+        delete returnedObject.passwordHash
     }
 })
 
