d416e693c40cc42fa57b59fdfb5a4ff7c9678ff4
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-03-18 12:28:50 +0300

    using userExtractor in routes

--- bloglist-backend/app.js
+++ bloglist-backend/app.js
@@ -23,7 +23,7 @@ mongoose
 
 app.use(express.json())
 
-app.use('/api/blogs', blogsRouter)
+app.use('/api/blogs', middleware.userExtractor, blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 app.use(middleware.unknownEndpoint)
