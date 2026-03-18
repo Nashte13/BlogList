ccf78ca6376f7e80956e7130e202492e5f60f298
Nahashon Mwangi <nashmwangi26@gmail.com>
2026-03-18 11:24:01 +0300

    correcting token extractor import

--- bloglist-backend/app.js
+++ bloglist-backend/app.js
@@ -22,13 +22,13 @@ mongoose
     })
 
 app.use(express.json())
-app.use(middleware.requestLogger)
-app.use(mongoose.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
+app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
 module.exports = app
